5b4803b4d79adfd643a82c00ee70a9cc35f7a00c
JeyaMaity <jeyamaity@gmail.com>
2026-04-05 14:00:49 +0530

    Create pie chart static.py

---
+++ pie chart static.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+
+labels = ['Python', 'Java', 'C++', 'JavaScript']
+sizes = [40, 25, 20, 15]
+colors = ['gold', 'lightblue', 'lightgreen', 'coral']
+
+
+plt.figure(figsize=(6,6))
+plt.pie(
+    sizes,
+    labels=labels,
+    colors=colors,
+    autopct='%1.1f%%',  
+    startangle=90        
+)
+
+plt.title("Programming Language Popularity")
+plt.axis('equal')  
+
+plt.show()
